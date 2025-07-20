4fa8693d7a5433fdb47f4011d955b0c0cefb9889
arpran <arpran@gmail.com>
2025-07-20 14:52:08 +1000

    Day 10 of 50 Python Script Challenge
    Day 10 of 50 Python Script Challenge - Name List

---
+++ namlist.py
@@ -0,0 +1,8 @@
+# Name List Script
+
+# Store 5 names in a list
+names = ["Arul", "Alex Kumar", "Veronica", "Vijay", "Mary"]
+
+# Print each name with its length
+for name in names:
+    print(f"Name: {name}, Length: {len(name)}")
